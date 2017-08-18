a16113d37d98a0247789ac881e7fa9c0affc6d7c
Administrator <bian@test>
2017-08-18 18:37:22 +0800

    add for rest framework

--- illidan/polls/models.py
+++ illidan/polls/models.py
@@ -1,8 +1,11 @@
+# coding=utf-8
 import datetime
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
+from pygments.lexers import get_all_lexers         # 一个实现代码高亮的模块
+from pygments.styles import get_all_styles
 
 
 @python_2_unicode_compatible
@@ -31,3 +34,22 @@ class Choice(models.Model):
         return self.choice_text
 
 
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])   # 得到所有编程语言的选项
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())       # 列出所有配色风格
+
+
+@python_2_unicode_compatible
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('created',)
