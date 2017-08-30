8c537d154cdc9a0c73cf0d3250d66048b5e35162
Administrator <bian@test>
2017-08-30 16:50:36 +0800

    add ansible api test

--- illidan/ansible_api/tests.py
+++ illidan/ansible_api/tests.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from .ansible_api import Api, ModuleRunner
+
 # Create your tests here.
+
+api = Api("localhost,")
+module_runner = ModuleRunner("shell")
+module_runner.hookup(api)
+module_runner.execute("ls", "-alF")
+
